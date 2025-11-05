ab6a4d5590974764232c721f06341456a9622ed5
kavita <kavitasoren2000@gmail.com>
2025-11-05 23:52:35 +0530

    dynamic data table

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://dynamicdatatables.netlify.app"
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://dynamic-data-table-assignment.onrender.com"
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
